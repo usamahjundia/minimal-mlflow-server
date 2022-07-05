74d34022e22e1cf01be216ee8ea37982f805bd4b
usamahjundia <usamahjundia@hotmail.com>
2022-07-05 15:52:24 +0700

    crlf issue fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mlflow server\
- --backend-store-uri "postgresql://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"\
- --default-artifact-root mlflow-artifacts:/home/mlflow/mlartifacts \
- --serve-artifacts \
- --host ${MLFLOW_HOST} \
- --port ${MLFLOW_PORT}
+    --backend-store-uri "postgresql://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"\
+    --default-artifact-root mlflow-artifacts:/home/mlflow/mlartifacts \
+    --serve-artifacts \
+    --host ${MLFLOW_HOST} \
+    --port ${MLFLOW_PORT}
